6fdc06f1b5a5cdb19561c4d8be1a4f50db431ca6
Rocked03 <rockedohthree@gmail.com>
2025-08-24 02:05:18 +1200

    fix: pnpm

--- src/components/polls/forms/choices/index.ts
+++ src/components/polls/forms/choices/index.ts
@@ -5,6 +5,3 @@ export { ChoicesSkeleton } from "./ChoicesSkeleton";
 export { Choices } from "./Choices";
 export { ChoiceLabelMap, MAX_CHOICES } from "./constants";
 export * from "./styles";
-
-// Re-export for backward compatibility
-export { ChoicesSkeleton as ChoicesSkeletonComponent } from "./ChoicesSkeleton";
