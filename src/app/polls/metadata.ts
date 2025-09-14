5c630d8df0fda746f807dbd46cda862d925a66fe
Rocked03 <rockedohthree@gmail.com>
2025-09-15 01:18:33 +1200

    feat: metadata

---
+++ src/app/polls/metadata.ts
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+
+export const pollsMetadata: Metadata = {
+  title: "Marvel Discord Polls",
+  description: "Vote on Marvel polls – created by the Marvel Discord",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
+  },
+  openGraph: {
+    type: "website",
+    title: "Marvel Discord Polls",
+    description: "Vote on Marvel polls – created by the Marvel Discord",
+    siteName: "Marvel Discord",
+    images: [
+      relativeImagePathToAbsolute(
+        "/img/gallery/banner/Marvel Discord Banner.png"
+      ),
+    ],
+  },
+};
